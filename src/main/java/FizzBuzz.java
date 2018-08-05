c2e97022052726ca609bfd6739e41d73d5fdc263
RuipengWang <>
2018-08-05 15:13:33 +0800

    Refactor: 旧的不变，新的创建 -- Ruipeng Wang

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -30,4 +30,22 @@ class FizzBuzz {
 
         return String.valueOf(input);
     }
+
+    String sayRefactored(int input) {
+        String result = "";
+
+        if(input % 3 == 0){
+            result += "Fizz";
+        }
+
+        if(input % 5 == 0){
+            result += "Buzz";
+        }
+
+        if(input % 7 == 0){
+            result += "Whizz";
+        }
+
+        return result.equals("") ? String.valueOf(input) : result;
+    }
 }
