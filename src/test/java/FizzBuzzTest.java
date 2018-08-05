d8a365456ae33a88cfcd053a61ad5c36f72b0803
RuipengWang <>
2018-08-05 14:55:41 +0800

    Refactor test case code to create before each setup method -- Ruipeng Wang

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,23 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    private void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void should_return_1_when_input_is_1() {
-       FizzBuzz fizzBuzz = new FizzBuzz();
-       assertThat(fizzBuzz.say(1)).isEqualTo("1");
+        assertThat(fizzBuzz.say(1)).isEqualTo("1");
     }
 
     @Test
     void should_return_fizz_when_input_is_3() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.say(3)).isEqualTo("Fizz");
     }
 
     @Test
     void should_return_fizz_when_input_is_6() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.say(6)).isEqualTo("Fizz");
     }
 }
