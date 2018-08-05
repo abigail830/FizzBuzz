662948c5f6f92306528540ef9d006a7bca21a0d0
RuipengWang <>
2018-08-05 15:16:00 +0800

    Refactor: 旧的再见 -- Ruipeng Wang

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,36 +1,4 @@
 class FizzBuzz {
-    String sayOld(int input) {
-        if(input % 3 == 0 && input % 5 == 0 && input % 7 == 0){
-            return "FizzBuzzWhizz";
-        }
-
-        if(input % 3 == 0 && input % 5 == 0){
-            return "FizzBuzz";
-        }
-
-        if(input % 3 == 0 && input % 7 == 0){
-            return "FizzWhizz";
-        }
-
-        if(input % 5 == 0 && input % 7 == 0){
-            return "BuzzWhizz";
-        }
-
-        if(input % 3 == 0){
-            return "Fizz";
-        }
-
-        if(input % 5 == 0){
-            return "Buzz";
-        }
-
-        if(input % 7 == 0){
-            return "Whizz";
-        }
-
-        return String.valueOf(input);
-    }
-
     String say(int input) {
         String result = "";
 
