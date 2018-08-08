99eadf819f194168d2da6fe311785829493aecd7
RuipengWang <>
2018-08-09 00:07:12 +0800

    Add easier solution for FizzBuzzWhizz -- Ruipeng Wang

--- src/main/Haskell/FizzBuzz.hs
+++ src/main/Haskell/FizzBuzz.hs
@@ -4,3 +4,7 @@ say a
 	where result = [output|input <- [a], input `mod` 3 == 0, let output = "Fizz"] ++
 				   [output|input <- [a], input `mod` 5 == 0, let output = "Buzz"] ++
 				   [output|input <- [a], input `mod` 7 == 0, let output = "Whizz"]
+
+say' input = if null desc then show input else desc where
+   desc = concat [label | (diviedNumber,label) <- tags, input `mod` diviedNumber == 0]
+   tags = [ (3,"Fizz"), (5,"Buzz"), (7,"Whizz") ]
